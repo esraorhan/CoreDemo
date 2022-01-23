497769f2bc5f8c99b6b8795d56764d29229ee7d2
esra orhan <esraorhan333@hotmail.com>
2022-01-23 18:54:12 +0300

    30.Ders View Component Yapısı işlendi

--- DataAccessLayer/EntityFramework/EfBlogRepository.cs
+++ DataAccessLayer/EntityFramework/EfBlogRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +11,18 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfBlogRepository: GenericRepository<Blog> ,IBlogDal
+    public class EfBlogRepository : GenericRepository<Blog>, IBlogDal
     {
+        public List<Blog> GetListWithCategory()
+        {
+            using(var c = new Context())
+            { 
+                //beraberinde ilişkili olduğu tabloyu da getirmesini istedik.
+                //ilk önce abstract klosörğnde list olarak metot tanımlandı önceki adım..
+                //daha sonrasında repositories klasöründe blogrepository sildik.çünkü generic kullanmaya başladık.
+                return c.Blogs.Include(x => x.Category).ToList();
+            }
+           
+        }
     }
 }
