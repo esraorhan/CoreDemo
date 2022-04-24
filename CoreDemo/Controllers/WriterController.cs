eac1b376c2685ec9859f09cb6cc6206adc3b3638
esra orhan <esraorhan333@hotmail.com>
2022-04-24 16:51:05 +0300

    72.ders işlendi

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace CoreDemo.Controllers
 {
     //[Authorize]  => config de proje bazlı authorize işlemi yapmış olduk. startup. cs de tamamn
     public class WriterController : Controller
     {
+        WriterManager vm = new WriterManager(new EfWriterRepository());
         public IActionResult Index()
         {
             return View();
@@ -38,6 +45,57 @@ namespace CoreDemo.Controllers
         public PartialViewResult WriterFooterPartial()
         {
             return PartialView();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult WriterEditProfile()
+        {
+            var writervalues = vm.TGetById(1);
+            return View(writervalues);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult WriterEditProfile(Writer p,string AgainPassword)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult results = wl.Validate(p);
+            if (results.IsValid)
+            {
+                if (p.WriterPassword == AgainPassword)
+                {
+                    vm.TUpdate(p);
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    ViewBag.Hata = "Şifreler Uyumsuz kontrol ediniz.";
+                    //return RedirectToAction("WriterEditProfile", "Writer");
+                }
+               
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(Writer p)
+        {
+            vm.TAdd(p);
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
