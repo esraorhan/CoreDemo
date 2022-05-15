3c1a9dfee0c6b1ca0248ec78a428cc8bc812cfbf
esra orhan <esraorhan333@hotmail.com>
2022-05-15 22:57:04 +0300

    84. ders işlendi.

---
+++ CoreDemo/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class MessageController : Controller
+    {
+        //mesajların tamamını göstermk için açıldı.
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        [AllowAnonymous]
+        public IActionResult InBox()
+        {
+            int id = 3;
+
+            var values = mm.GetInboxListByWriter(id);
+            return View(values);
+        }
+
+       
+        public IActionResult MessageDetails(int id)
+        {
+            var messagevalue = mm.TGetById(id);
+            return View(messagevalue);
+        }
+    }
+}
--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -10,13 +10,13 @@ namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterMessageNotification : ViewComponent
     {
-        MessageManager mm = new MessageManager(new EfMessageRepository());
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
 
         public IViewComponentResult Invoke()
         {
-            string p;
-            p = "ali@gmail.com"; 
-            var values = mm.GetInboxListByWriter(p);
+            int id =3;
+           
+            var values = mm.GetInboxListByWriter(id);
             return View(values);
         }
     }
