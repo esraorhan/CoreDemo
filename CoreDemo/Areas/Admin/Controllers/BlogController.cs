8f371cf7c04d6777cbc1f3a9854c8dd3812fb4f0
esraorhan <esraorhan333@gmail.com>
2022-06-07 17:20:00 +0300

    97.Ders Tamamlandı.

---
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,102 @@
+﻿using ClosedXML.Excel;
+using CoreDemo.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")] //bildirmek gerekiyor..
+    public class BlogController : Controller
+    {
+        //ClosedXml paketini kurduk..
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "BlogID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;// 1 satıra başlık belirledik.
+                foreach (var item in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName ;
+                    BlogRowCount++;
+                }
+
+                using(var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vdn.openxmlformats-officedocument.spreadsheetml.sheet","CshapileExcel.xlsx");
+                }
+            }
+           // return View();
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>
+            {
+                new BlogModel{ID=1,BlogName="C# Programlamaya Giriş"},
+                new BlogModel{ID=2,BlogName="Test Firmasının Araçları"},
+                new BlogModel{ID=3,BlogName="2022 Olimpiyatları"}
+            };
+
+            return bm;
+        }
+
+        public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+
+
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "BlogID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;// 1 satıra başlık belirledik.
+                foreach (var item in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vdn.openxmlformats-officedocument.spreadsheetml.sheet", "CshapileExcel.xlsx");
+                }
+            }
+        }
+
+        public List<BlogModel2> BlogTitleList()
+        {
+            List<BlogModel2> blogs = new List<BlogModel2>();
+
+            using(var c = new Context())
+            {
+                blogs = c.Blogs.Select(c => new BlogModel2 { ID = c.BlogID, BlogName = c.BlogTitle }).ToList();
+            }
+            return blogs;
+        }
+
+        public IActionResult BlogTitleListExcel()
+        {
+            return View();
+        }
+    }
+}
--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -23,5 +26,48 @@ namespace CoreDemo.Areas.Admin.Controllers
             var values = cm.GetList().ToPagedList(page,3);
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult result = cv.Validate(p); //p den gelen değerleri validasyon sınfından kontrolünü sağla demek. 
+            if (result.IsValid)
+            {
+                //ekleme işlemi yapılırken httpget ve httppost attributelerinin tanımlandığı  metotların isimleri aynı olmak zorundadır. 
+                //httpget sayfa yüklenirken 
+                //submit olayınd a post işlemi çalısır. 
+                // Httpget attrubte komutu sayfada kategorize veya benzeri işlemler kullanılırken sayfa yüklendiği anda listlelenmesi isteenn niteliklerde kullanılabilir. 
+               
+                p.CategoryStatus = true;             
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = cm.TGetById(id);
+            cm.TDelete(value);
+            return RedirectToAction("Index");
+        }
     }
+
 }
