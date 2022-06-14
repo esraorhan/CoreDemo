4a07744925354819de93d8ce530ae1b79a46e59c
esraorhan <esraorhan333@gmail.com>
2022-06-14 15:24:51 +0300

    115.ders bitti. Default get api denendi.

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //connecting string tanımlanacak yer...
+        {
+            //  optionsBuilder.UseSqlServer("server=DESKTOP-8AK4CAP;database=CoreBlogDb; integrated security= true;");
+            optionsBuilder.UseSqlServer("server=ESRAORHAN;database=CoreBlogApiDb; integrated security= true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
