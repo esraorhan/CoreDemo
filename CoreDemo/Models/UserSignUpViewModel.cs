fa7acfd8ec94f833cfab0ba4ba7a3595e4614ae2
esraorhan <esraorhan333@gmail.com>
2022-06-17 14:30:03 +0300

    124.Ders Tamamlandı. İdentity ile Register işlemi

---
+++ CoreDemo/Models/UserSignUpViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage ="Lütfen Ad Soyad Giriniz!")]
+        public string nameSurname { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Lütfen Şifre Giriniz!")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrar")]
+        [Compare("Password",ErrorMessage ="Şifreler Eşleşmiyor")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Display(Name = "Mail")]
+        [Required(ErrorMessage = "Lütfen Mail Giriniz!")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Giriniz!")]
+        public string UserName { get; set; }
+        public bool Tick { get; set; }
+    }
+}
