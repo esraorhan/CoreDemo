b09b388159be61c48d92cf45350f3c464721722d
esraorhan <esraorhan333@gmail.com>
2022-06-21 14:00:08 +0300

    137. ders Admin Blog Listesi

---
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
+        public IActionResult InBox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+
+            var values = mm.GetInboxListByWriter(writerID);
+            ViewBag.InBoxCount = values.Count;
+            return View(values);
+        }
+
+        public IActionResult SendBox()
+        {
+
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+
+            var values = mm.GetSendBoxListByWriter(writerID);
+            ViewBag.SendBoxCount = values.Count;
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult ComposeMessage()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ComposeMessage(Messaage2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            p.SenderID = writerID;
+            p.ReceiverID = 2;
+            p.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.MessageStatus = true;
+            mm.TAdd(p);
+            return RedirectToAction("SendBox", "AdminMessage");
+        }
+    }
+}
